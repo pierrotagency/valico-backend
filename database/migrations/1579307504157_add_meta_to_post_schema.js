efb925f2513fd43eef4f66cd728eb9855995481b
rodrigobutta <rbutta@gmail.com>
2020-01-17 21:38:26 -0300

    post migration add metsa

---
+++ database/migrations/1579307504157_add_meta_to_post_schema.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class AddMetaToPostSchema extends Schema {
+  up () {
+    this.table('posts', (table) => {
+      // alter table
+      table.string('meta_title',200)
+      table.string('meta_description',400)
+      table.string('meta_keywords',200)
+      table.string('meta_image',200)
+    })
+  }
+
+  down () {
+    this.table('posts', (table) => {
+      // reverse alternations
+      table.dropColumn('meta_title')
+      table.dropColumn('meta_description')
+      table.dropColumn('meta_keywords')
+      table.dropColumn('meta_image')
+    })
+  }
+}
+
+module.exports = AddMetaToPostSchema
